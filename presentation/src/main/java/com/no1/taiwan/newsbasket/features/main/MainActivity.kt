7df4a8bb96be50acef23517879192ff7d0902f45
Jieyi <pokkbaby@gmail.com>
2018-11-20 19:49:29 +0900

    Feature: separated the difference category resource files.

--- presentation/src/main/java/com/no1/taiwan/newsbasket/features/main/MainActivity.kt
+++ presentation/src/main/java/com/no1/taiwan/newsbasket/features/main/MainActivity.kt
@@ -1,21 +1,10 @@
 package com.no1.taiwan.newsbasket.features.main
 
 import android.os.Bundle
-import com.devrapid.kotlinknifer.loge
-import com.devrapid.kotlinknifer.logw
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.no1.taiwan.newsbasket.R
 import com.no1.taiwan.newsbasket.bases.AdvActivity
-import com.no1.taiwan.newsbasket.ext.doWith
-import com.no1.taiwan.newsbasket.ext.happenError
-import com.no1.taiwan.newsbasket.ext.observeNonNull
-import com.no1.taiwan.newsbasket.ext.peel
-import kotlinx.android.synthetic.main.activity_main.btn_click
-import kotlinx.android.synthetic.main.activity_main.et_input1
-import kotlinx.android.synthetic.main.activity_main.et_input2
 import kotlinx.android.synthetic.main.activity_main.navigation
-import kotlinx.android.synthetic.main.activity_main.tv_res
-import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AdvActivity<MainViewModel>() {
     private val navigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -38,17 +27,12 @@ class MainActivity : AdvActivity<MainViewModel>() {
 
     override fun init(savedInstanceState: Bundle?) {
         navigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
-        observeNonNull(vm.newsLiveData) {
-            peel { logw(it) } happenError { loge(it) } doWith this@MainActivity
-        }
-        observeNonNull(vm.tokenLiveData) {
-            peel { vm::keepToken } happenError { loge(it) } doWith this@MainActivity
-        }
-
-        btn_click.onClick {
-            val s = "${et_input1.text} + ${et_input2.text}"
-            tv_res.text = s
-        }
+//        observeNonNull(vm.newsLiveData) {
+//            peel { logw(it) } happenError { loge(it) } doWith this@MainActivity
+//        }
+//        observeNonNull(vm.tokenLiveData) {
+//            peel { vm::keepToken } happenError { loge(it) } doWith this@MainActivity
+//        }
     }
 
     override fun provideLayoutId() = R.layout.activity_main
