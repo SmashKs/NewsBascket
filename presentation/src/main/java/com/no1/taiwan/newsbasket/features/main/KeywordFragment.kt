6eb08209ddf11ba581af5573a614cf217a32a691
Jieyi <pokkbaby@gmail.com>
2018-11-21 08:51:05 +0900

    Feature: added the recycler view object component into keywords fragment.

--- presentation/src/main/java/com/no1/taiwan/newsbasket/features/main/KeywordFragment.kt
+++ presentation/src/main/java/com/no1/taiwan/newsbasket/features/main/KeywordFragment.kt
@@ -2,16 +2,25 @@ package com.no1.taiwan.newsbasket.features.main
 
 import android.os.Bundle
 import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.no1.taiwan.newsbasket.R
 import com.no1.taiwan.newsbasket.bases.BaseFragment
+import com.no1.taiwan.newsbasket.internal.di.tags.ObjectLabel.LINEAR_LAYOUT_VERTICAL
+import kotlinx.android.synthetic.main.fragment_keyword.fab_add
+import kotlinx.android.synthetic.main.fragment_keyword.rv_keywords
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.kodein.di.generic.instance
 
 class KeywordFragment : BaseFragment<MainActivity>() {
+    private val linearLayout by instance<LinearLayoutManager>(LINEAR_LAYOUT_VERTICAL)
     /**
      * Initialize method.
      *
      * @param savedInstanceState before status.
      */
     override fun rendered(savedInstanceState: Bundle?) {
+        componentSetting()
+        eventSetting()
     }
 
     /**
@@ -27,4 +36,20 @@ class KeywordFragment : BaseFragment<MainActivity>() {
      * @return [String] action bar title.
      */
     override fun actionBarTitle() = "Keywords"
+
+    private fun componentSetting() {
+        rv_keywords.apply {
+            layoutManager = linearLayout
+//            adapter =
+        }
+    }
+
+    private fun eventSetting() {
+        fab_add.onClick {
+            createKeywordDialog()
+        }
+    }
+
+    private fun createKeywordDialog() {
+    }
 }
