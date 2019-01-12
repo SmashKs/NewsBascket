94e9a19d2f4462db732d496d5c38009470946873
Jieyi <pokkbaby@gmail.com>
2019-01-12 21:08:50 +0900

    Feature: added new use cases into di system.

--- presentation/src/main/java/com/no1/taiwan/newsbasket/internal/di/dependency/UsecaseModule.kt
+++ presentation/src/main/java/com/no1/taiwan/newsbasket/internal/di/dependency/UsecaseModule.kt
@@ -2,6 +2,9 @@ package com.no1.taiwan.newsbasket.internal.di.dependency
 
 import com.no1.taiwan.newsbasket.domain.usecases.AddSubscriberWrapUsecase
 import com.no1.taiwan.newsbasket.domain.usecases.KeepNewsTokenWrapUsecase
+import com.no1.taiwan.newsbasket.domain.usecases.googlenews.FetchEverythingWrapUsecase
+import com.no1.taiwan.newsbasket.domain.usecases.googlenews.FetchNewsSourcesWrapUsecase
+import com.no1.taiwan.newsbasket.domain.usecases.googlenews.FetchTopNewsWrapUsecase
 import com.no1.taiwan.newsbasket.domain.usecases.keyword.AddKeywordRespUsecase
 import com.no1.taiwan.newsbasket.domain.usecases.keyword.AddLocalKeywordWrapUsecase
 import com.no1.taiwan.newsbasket.domain.usecases.keyword.DeleteKeywordRespUsecase
@@ -23,6 +26,10 @@ import org.kodein.di.generic.singleton
 object UsecaseModule {
     fun usecaseProvider() = Module("Use Cases") {
         //region For Fragments
+        bind<FetchTopNewsWrapUsecase>() with singleton { FetchTopNewsWrapUsecase(instance()) }
+        bind<FetchEverythingWrapUsecase>() with singleton { FetchEverythingWrapUsecase(instance()) }
+        bind<FetchNewsSourcesWrapUsecase>() with singleton { FetchNewsSourcesWrapUsecase(instance()) }
+
         bind<FetchRemoteNewsWrapUsecase>() with singleton { FetchRemoteNewsWrapUsecase(instance()) }
         bind<FetchLocalNewsWrapUsecase>() with singleton { FetchLocalNewsWrapUsecase(instance()) }
         bind<AddLocalNewsWrapUsecase>() with singleton { AddLocalNewsWrapUsecase(instance()) }
